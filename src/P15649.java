0bcb24535415f967331018b236d4e950d89fd4ec
munzii <munzii422@gmail.com>
2022-01-14 23:46:59 +0900

    studying

--- src/P15649.java
+++ src/P15649.java
@@ -3,15 +3,20 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class P15649 {
+	
+	public static int[] arr;
+	public static boolean[] visit;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
 	}
 
