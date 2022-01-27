fae6758619da8954e8ac4fe7891dfac69f538a33
munzii <munzii422@gmail.com>
2022-01-27 23:53:43 +0900

    studying

--- src/P15649.java
+++ src/P15649.java
@@ -37,6 +37,15 @@ public class P15649 {
 			bw.write('\n');
 			return;
 		}
+		
+		for(int i=0; i<n; i++) {
+			if(!visit[i]) {
+				visit[i] = true;
+				arr[depth] = i+1;
+				dfs(n,m,depth+1);
+				visit[i] = false;
+			}
+		}
 	}
 
 }
